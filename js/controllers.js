ed7a5664b80020f65d12a7517a584a5f3faccd6f
Benjamin Ratliff <ben@2bitcreative.com>
2015-12-21 19:10:12 -0500

    added no results logic

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,30 @@
 var userControllers = angular.module('userControllers', ['ngAnimate']);
 
-userControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+userControllers.controller('ListController', ['$scope', '$http', '$filter', function($scope, $http) {
   $http.get('js/users.json').success(function(data) {
     $scope.users = data;
-    $scope.userOrder = 'name';
+    $scope.userOrder = 'last_name';
   });
+
+  $scope.$watch('search', function(val)
+    { 
+        
+    });
+
 }]);
 
 userControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.doctors = data;
+  $http.get('js/users.json').success(function(data) {
+    $scope.users = data;
     $scope.whichItem = $routeParams.itemId;
 
     if ($routeParams.itemId > 0) {
       $scope.prevItem = Number($routeParams.itemId)-1;
     } else {
-      $scope.prevItem = $scope.doctors.length-1;
+      $scope.prevItem = $scope.users.length-1;
     }
 
-    if ($routeParams.itemId < $scope.doctors.length-1) {
+    if ($routeParams.itemId < $scope.users.length-1) {
       $scope.nextItem = Number($routeParams.itemId)+1;
     } else {
       $scope.nextItem = 0;
